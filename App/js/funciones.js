f39b2f852b6a6d8f5bd51fb9fc61cd01666787f8
jomariosantana <jomario.santanaan@elorrieta-errekamari.com>
2021-01-26 20:51:48 +0100

    imagenes

--- App/js/funciones.js
+++ App/js/funciones.js
@@ -1,25 +1,7 @@
-
-
-function crearJuego(nivel) {
-   
-    
-    if(nivel==1){
-        $('.juego').css({border:'2px solid black'}); 
-        for(var i=0; i<6;i++){
-
-            $('.juego').append("<div class='imagen'>"+i+"</div>"); 
-        }
-
-    
-    }if(nivel==2){
-        $('.juego').css({background:'blue', width:'500px',height:'500px',border:'2px solid black'});
-    }if(nivel==3){ 
-        // $('.juego').css("");
-        $('.juego').css({backgroundColor:'green', width:'500px',height:'500px',border:'2px solid black'});
-
-    }
-    
-}
+// $(document).load(inicio);
+  
+var array = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+var aleatorios = [];
 
 function aleatorizar(array) {
     var j, x, i;
@@ -33,4 +15,37 @@ function aleatorizar(array) {
     array[i] = array[j];
     array[j] = x;
     }
+    aleatorios = array;
+}
+
+function crearJuego(nivel){
+    $('.juego').html(''); //reset
+
+    aleatorizar(array);
+    for(var i=0; i<4; i++ ){
+
+        $('.juego').append("<div class='imagen'></div><div class='imagen'></div><div class='imagen'></div><div class='imagen'></div><div class='imagen'></div><br>");
+    }
+           
+    $('.juego').css({border:'2px solid black'});
+    // $('.imagen').css({backgroundColor:'black', width:'100px',height:'100px',border:'2px solid white'});
+
+    valores();
+
+
+} 
+
+function valores() {
+   console.log(aleatorios);
+
+   for(let i in aleatorios) {
+    $(`.imagen:eq(${i})`).attr("value",aleatorios[i]);
+   }
+    
+    
+}
+
+function meterimagenes() {
+
+    
 }
