445c64c8482a7fe2e985fc43017c3fd5fac4911d
jomariosantana <jomario.santanaan@elorrieta-errekamari.com>
2021-01-27 20:43:15 +0100

    crear flipar imagenes

--- App/js/funciones.js
+++ App/js/funciones.js
@@ -1,5 +1,11 @@
-// $(document).load(inicio);
-  
+$(document).ready(inicio);
+
+function inicio() {
+
+    // flipar();
+}
+
+
 var array = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
 var aleatorios = [];
 
@@ -18,30 +24,59 @@ function aleatorizar(array) {
     aleatorios = array;
 }
 
-function crearJuego(nivel){
+function crearJuego(){
+
     $('.juego').html(''); //reset
 
-    aleatorizar(array);
+    aleatorizar(array); // randomiza el array al elegir la dificultad
+
     for(var i=0; i<4; i++ ){
 
-        $('.juego').append("<div class='imagen'></div><div class='imagen'></div><div class='imagen'></div><div class='imagen'></div><div class='imagen'></div><br>");
+        $('.juego').append("<div class='imagen'></div><div class='imagen'></div><div class='imagen'></div><div class='imagen'></div><div class='imagen'></div><br>"); // añado los div que llevarán las imagenes
     }
-           
-    $('.juego').css({border:'4px solid black'});
-    // $('.imagen').css({backgroundColor:'black', width:'100px',height:'100px',border:'2px solid white'});
-
+    $('.imagen').css({backgroundImage:'url(\'./img/hp.jpg \')'});
+    $('.juego').css({border:'4px solid black'}); //aplico un border al juego al empezar
+    // atribuirImagenes();
     valores();
-
-
+    flipar(); // ejecuto la funcion valores
+    
 } 
 
-function valores() {
-   console.log(aleatorios);
+function atribuirImagenes(valor) {
 
-   for(let i in aleatorios) {
-    $(`.imagen:eq(${i})`).attr("value",aleatorios[i]);
-   }
-    
+    for( var i=1; i<21; i++){
+        var value = $(`.imagen:eq(${i})`).attr("value");
+        
+        console.log(value);
+        
+        if( value==valor){
+            console.log("match");
+            $(`.imagen:eq(${valor})`).css({backgroundImage:'url(\'./img/hp.jpg \')'});   
+        }
+
+    }
     
 }
 
+
+function valores() { // funcion que atribue valores del array a las imagenes
+
+   console.log(aleatorios);
+
+    for(let i in aleatorios) { // recogemos el array y atribuimos los valores
+        $(`.imagen:eq(${i})`).attr("value",aleatorios[i]);
+    }   
+}
+
+function flipar(){
+    
+    $(".imagen").click(function () {
+        var valor = $(this).attr("value");
+        $(this).css({backgroundImage:'url(\'./img/'+valor+'.jpg \')'},
+            setTimeout(function(){atribuirImagenes(valor)},2000)
+        );
+    });
+        
+    
+}
+// atribuirImagenes(valor)
